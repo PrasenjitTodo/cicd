6cc26a12cd816df1b5e09ccdda94a0a70dc5db6a
PrasenjitTodo <159232536+PrasenjitTodo@users.noreply.github.com>
2024-04-02 07:20:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-console.log('\n\n-: App Started :-');
+console.log('\n\n-: App Started 04:-');
 
 const express       = require('express');
 const app   = express();
 
 app.use('/', (req, res, next)=>{
-    console.log('-: Welcome 02:-');
-    res.send('-: Welcome 02:-');
+    console.log('-: Welcome 04:-');
+    res.send('-: Welcome 04:-');
 });
 
-console.log('-: App Running 03:-');
+console.log('-: App Running 04:-');
 app.listen(4000);
